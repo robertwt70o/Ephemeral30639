fd7d9c1fe234a39d7855e072d15f21625d2a3240
robertwt70o <ai35955220@sina.com>
2021-02-01 19:34:35 +0800

    create add and delete feature to taken course page

--- routes/takenCourse.js
+++ routes/takenCourse.js
@@ -22,8 +22,8 @@ const pool = mysql.createPool({
     database: "egcicourse"
 })
 
-
-router.get('/', checkAuthenticated, (req, res) =>{
+//load all taken course
+router.get('/loadtakencourse', checkAuthenticated, (req, res) =>{
     pool.getConnection(function(err, connection) {
       connection.query(`(SELECT Courses.ID, Courses.Name, Courses.Category, Courses.Credit FROM ${req.user.studentID}_Taken_Courses INNER JOIN Courses ON ${req.user.studentID}_Taken_Courses.Course_ID=Courses.ID)`, (err, takenCourses) => {
         if (err) throw err;
@@ -34,5 +34,68 @@ router.get('/', checkAuthenticated, (req, res) =>{
   })
 })
 
+//load all non taken course
+router.get('/loadcourselist', checkAuthenticated, (req, res) =>{
+  pool.getConnection(function(err, connection) {
+    connection.query(`SELECT Courses.ID, Courses.Name FROM Courses WHERE Courses.ID NOT IN (SELECT ${req.user.studentID}_Taken_Courses.Course_ID FROM ${req.user.studentID}_Taken_Courses)`, (err, Courses) => {
+      if (err) throw err;
+      console.log(Courses);
+      res.send(Courses) 
+      connection.release(); 
+    })
+  })
+})
+
+//add taken course
+router.get('/addtakencourse/:courseID', checkAuthenticated, (req, res) =>{
+  pool.getConnection(function(err, connection) {
+    connection.query(`INSERT INTO ${req.user.studentID}_Taken_Courses (Student_ID, Course_ID) VALUES ('${req.user.studentID}', '${req.params.courseID}')`, (err) => {
+      if (err){
+        console.log(err.sqlMessage)
+        res.send(err.sqlMessage)
+        return
+      }
+      else{
+        connection.query(`INSERT INTO Taken_Courses (Student_ID, Course_ID) VALUES ('${req.user.studentID}', '${req.params.courseID}')`, (err) => {
+          if (err){
+            console.log(err.sqlMessage)
+            res.send(err.sqlMessage)
+            return
+          } 
+          console.log("added " + req.user.studentID + ", " + req.params.courseID + " to Taken_Courses table")
+        })
+        console.log("added "  + req.params.courseID + " to " + req.user.studentID + "'s taken course table");
+        res.send("add success") 
+      }
+      connection.release(); 
+    })
+  })
+})
+
+//delete taken course
+router.delete('/deletetakencourse/:courseID', checkAuthenticated, (req, res) =>{
+  pool.getConnection(function(err, connection) {
+    connection.query(`DELETE FROM ${req.user.studentID}_Taken_Courses where Course_ID like '${req.params.courseID}'`, (err) => {
+      if (err){
+        console.log(err.sqlMessage)
+        res.send(err.sqlMessage)
+        return
+      }
+      else{
+        connection.query(`DELETE FROM Taken_Courses where Student_ID like '${req.user.studentID}' and Course_ID like '${req.params.courseID}'`, (err) => {
+          if (err){
+            console.log(err.sqlMessage)
+            res.send(err.sqlMessage)
+            return
+          } 
+          console.log("deleted " + req.user.studentID + ", " + req.params.courseID + " from Taken_Courses table")
+        })
+        console.log("deleted "  + req.params.courseID + " from " + req.user.studentID + "'s taken course table");
+        res.send("delete success") 
+      }
+      connection.release(); 
+    })
+  })
+})
 
 module.exports = router
