e6774ba575ba72f35c88a76e7d50c53cf30cc559
robertwt70o <ai35955220@sina.com>
2021-02-19 08:13:25 +0800

    refined getcurrenttrimester.js endpoints

--- routes/getCurrentTrimester.js
+++ routes/getCurrentTrimester.js
@@ -14,12 +14,23 @@ const pool = mysql.createPool({
     database: "egcicourse"
 })
 
-router.get("/", checkAuthenticated, (req, res) => {
+router.get("/timetable", checkAuthenticated, (req, res) => {
     pool.getConnection(function(err, connection) {
         connection.query(`SELECT T2_2020_2021.ID, Name, T2_2020_2021.Date, T2_2020_2021.Time FROM T2_2020_2021 INNER JOIN Courses ON T2_2020_2021.ID=Courses.ID`, (err, currentTrimester) => {
           if (err) throw err;
           console.log(currentTrimester);
           res.send(currentTrimester) 
+          connection.release(); 
+        })
+    })
+})
+
+router.get("/studentcurrentenrollment", checkAuthenticated, (req, res) =>{
+    pool.getConnection(function(err, connection) {
+        connection.query(`SELECT T2_2020_2021.ID, Name, T2_2020_2021.Date, T2_2020_2021.Time FROM T2_2020_2021_Enrollment inner join T2_2020_2021 on T2_2020_2021_Enrollment.Course_ID=T2_2020_2021.ID inner join Courses ON T2_2020_2021.ID=Courses.ID where Student_ID like '${req.user.studentID}';`, (err, currentEnrollment) => {
+          if (err) throw err;
+          console.log(currentEnrollment);
+          res.send(currentEnrollment) 
           connection.release(); 
         })
     })
