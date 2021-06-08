48240a1eab5484ef93fb2a81c014936e0ad9b945
robertwt70o <ai35955220@sina.com>
2021-06-08 17:10:49 +0800

    Taken Courses Reworked

--- routes/takenCourse.js
+++ routes/takenCourse.js
@@ -8,7 +8,7 @@ const checkNotAuthenticated = authMethods.checkNotAuthenticated
 //Allow Access-Control-Allow-Origin
 //ref: https://stackoverflow.com/questions/18642828/origin-origin-is-not-allowed-by-access-control-allow-origin
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://scpm2021.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -24,7 +24,45 @@ const pool = mysql.createPool({
 
 const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }
 
-//load all taken course
+//load major category structure
+router.get('/loadmajorcategorystructure', checkAuthenticated, (req, res) =>{
+  pool.getConnection(function(err, connection) {
+    connection.query(`(SELECT MainCategory_structure.name FROM MainCategory_structure WHERE ${req.user.studentID.substring(0,2)} BETWEEN MainCategory_structure.start_year AND MainCategory_structure.end_year)`, (err, majorCategory) => {
+      if (err){
+        console.log(err)
+        res.send('Error')
+        connection.release()
+        return
+      };
+      const time = new Date(Date.now())
+      const timestamp = `${time.toLocaleDateString('en-US', options).substring(5)} ${time.toLocaleTimeString('en-US', {hour12: false})}`
+      console.log(`${timestamp}: sending all taken courses`)
+      res.send(majorCategory) 
+      connection.release(); 
+    })
+  })
+})
+
+//load sub category structure
+router.get('/loadsubcategorystructure', checkAuthenticated, (req, res) =>{
+  pool.getConnection(function(err, connection) {
+    connection.query(`(SELECT SubCategory_structure.name, SubCategory_structure.mainCategory, SubCategory_structure.totalcredit, SubCategory_structure.totalCreditAdvance, SubCategory_structure.groupbyremark FROM SubCategory_structure WHERE ${req.user.studentID.substring(0,2)} BETWEEN SubCategory_structure.start_year AND SubCategory_structure.end_year)`, (err, subCategory) => {
+      if (err){
+        console.log(err)
+        res.send('Error')
+        connection.release()
+        return
+      };
+      const time = new Date(Date.now())
+      const timestamp = `${time.toLocaleDateString('en-US', options).substring(5)} ${time.toLocaleTimeString('en-US', {hour12: false})}`
+      console.log(`${timestamp}: sending all taken courses`)
+      res.send(subCategory) 
+      connection.release(); 
+    })
+  })
+})
+
+//load all taken courses
 router.get('/loadtakencourse', checkAuthenticated, (req, res) =>{
     pool.getConnection(function(err, connection) {
       connection.query(`(SELECT ${req.user.studentID.substring(0,2)}Courses.ID, ${req.user.studentID.substring(0,2)}Courses.Name, ${req.user.studentID.substring(0,2)}Courses.Category, ${req.user.studentID.substring(0,2)}Courses.Credit, ${req.user.studentID}_Taken_Courses.remark FROM ${req.user.studentID}_Taken_Courses INNER JOIN ${req.user.studentID.substring(0,2)}Courses ON ${req.user.studentID}_Taken_Courses.Course_ID=${req.user.studentID.substring(0,2)}Courses.ID)`, (err, takenCourses) => {
@@ -43,10 +81,10 @@ router.get('/loadtakencourse', checkAuthenticated, (req, res) =>{
     })
 })
 
-//load all non taken course
-router.get('/loadcourselist', checkAuthenticated, (req, res) =>{
+//load all non taken courses
+router.get('/loadnontakencourse', checkAuthenticated, (req, res) =>{
   pool.getConnection(function(err, connection) {
-    connection.query(`SELECT ${req.user.studentID.substring(0,2)}Courses.ID, ${req.user.studentID.substring(0,2)}Courses.Name FROM ${req.user.studentID.substring(0,2)}Courses WHERE ${req.user.studentID.substring(0,2)}Courses.ID NOT IN (SELECT ${req.user.studentID}_Taken_Courses.Course_ID FROM ${req.user.studentID}_Taken_Courses)`, (err, Courses) => {
+    connection.query(`SELECT ${req.user.studentID.substring(0,2)}Courses.ID, ${req.user.studentID.substring(0,2)}Courses.Name, ${req.user.studentID.substring(0,2)}Courses.Category, ${req.user.studentID.substring(0,2)}Courses.Credit FROM ${req.user.studentID.substring(0,2)}Courses WHERE ${req.user.studentID.substring(0,2)}Courses.ID NOT IN (SELECT ${req.user.studentID}_Taken_Courses.Course_ID FROM ${req.user.studentID}_Taken_Courses)`, (err, Courses) => {
       if (err){
         console.log(err)
         res.send('Error')
@@ -63,55 +101,65 @@ router.get('/loadcourselist', checkAuthenticated, (req, res) =>{
 })
 
 //add taken course
-router.get('/addtakencourse/:courseID/:remark', checkAuthenticated, (req, res) =>{
-  pool.getConnection(function(err, connection) {
-    connection.query(`INSERT INTO ${req.user.studentID}_Taken_Courses (Student_ID, Course_ID, remark) VALUES ('${req.user.studentID}', '${req.params.courseID}', '${req.params.remark}')`, (err) => {
-      if (err){
-        console.log(err.sqlMessage)
-        res.send(err.sqlMessage)
-        return
-      }
-      else{
-        connection.query(`INSERT INTO Taken_Courses (Student_ID, Course_ID) VALUES ('${req.user.studentID}', '${req.params.courseID}')`, (err) => {
-          if (err){
-            console.log(err.sqlMessage)
-            res.send(err.sqlMessage)
-            return
-          } 
-          console.log("added " + req.user.studentID + ", " + req.params.courseID + " to Taken_Courses table")
-        })
-        console.log("added "  + req.params.courseID + " to " + req.user.studentID + "'s taken course table");
-        res.send("add success") 
-      }
-      connection.release(); 
-    })
-  })
+router.post('/addtakencourse', checkAuthenticated, (req, res) =>{
+  for (var i = 0; i < req.body.length; i++)  
+      addCourse(req.body[i].ID, req.user.studentID, req.body[i].remark)
+  res.send("add success") 
 })
 
 //delete taken course
-router.delete('/deletetakencourse/:courseID', checkAuthenticated, (req, res) =>{
+router.post('/deletetakencourse', checkAuthenticated, (req, res) =>{
+  for (var i = 0; i < req.body.length; i++)  
+    deleteCourse(req.body[i].ID, req.user.studentID)
+  res.send("delete success") 
+})
+
+function addCourse(courseID, studentID, remark){
   pool.getConnection(function(err, connection) {
-    connection.query(`DELETE FROM ${req.user.studentID}_Taken_Courses where Course_ID like '${req.params.courseID}'`, (err) => {
+    connection.query(`INSERT INTO ${studentID}_Taken_Courses (Student_ID, Course_ID, remark) VALUES ('${studentID}', '${courseID}', '${remark}')`, (err) => {
       if (err){
         console.log(err.sqlMessage)
         res.send(err.sqlMessage)
         return
       }
       else{
-        connection.query(`DELETE FROM Taken_Courses where Student_ID like '${req.user.studentID}' and Course_ID like '${req.params.courseID}'`, (err) => {
+        connection.query(`INSERT INTO Taken_Courses (Student_ID, Course_ID) VALUES ('${studentID}', '${courseID}')`, (err) => {
           if (err){
             console.log(err.sqlMessage)
             res.send(err.sqlMessage)
             return
           } 
-          console.log("deleted " + req.user.studentID + ", " + req.params.courseID + " from Taken_Courses table")
+          console.log("Added " + studentID + ", " + courseID + " to Taken_Courses table")
         })
-        console.log("deleted "  + req.params.courseID + " from " + req.user.studentID + "'s taken course table");
-        res.send("delete success") 
+        console.log("Added "  + courseID + " to " + studentID + "'s taken course table");
       }
       connection.release(); 
     })
   })
-})
+}
+
+function deleteCourse(courseID, studentID){
+  pool.getConnection(function(err, connection) {
+    connection.query(`DELETE FROM ${studentID}_Taken_Courses where Course_ID like '${courseID}'`, (err) => {
+      if (err){
+        console.log(err.sqlMessage)
+        res.send(err.sqlMessage)
+        return
+      }
+      else{
+        connection.query(`DELETE FROM Taken_Courses where Student_ID like '${studentID}' and Course_ID like '${courseID}'`, (err) => {
+          if (err){
+            console.log(err.sqlMessage)
+            res.send(err.sqlMessage)
+            return
+          } 
+          console.log("Deleted " + studentID + ", " + courseID + " from Taken_Courses table")
+        })
+        console.log("Deleted "  + courseID + " from " + studentID + "'s taken course table");
+      }
+      connection.release(); 
+    })
+  })
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(methodOverride('_method'))
 // I am not sure why, but there will be CORS error if this "use" is deleted.
 app.use(
     cors({
-         origin: "https://scpm2021.herokuapp.com", // allow to server to accept request from different origin
+         origin: "http://localhost:3000", // allow to server to accept request from different origin
          methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
          credentials: true, // allow session cookie from browser to pass through
    })
